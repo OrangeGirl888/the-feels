8e981a19653073501c645923e97d6e94421600a5
orangegirl888 <elline_bacalanmo@yahoo.com>
2016-08-31 12:02:38 +0000

    Updated html and stylesheet

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -50,7 +50,7 @@ class PoemsController < ApplicationController
   def update
     respond_to do |format|
       if @poem.update(poem_params)
-        format.html { redirect_to @poem, notice: 'That revision was rocked!' }
+        format.html { redirect_to @poem, notice: 'You rock! That revision was awesome!' }
         format.json { render :show, status: :ok, location: @poem }
       else
         format.html { render :edit }
